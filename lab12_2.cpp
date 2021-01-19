1dfa249051ba0bddb16a8636ee67ed7fa4906f45
ford <tewarad2020>
2021-01-19 16:34:24 +0700

    code completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,3 +21,27 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool im[][M]){
+    for(int i=0;i<32;i++) {
+        if (i != 0 && i != 31) cout << "|"; 
+        for(int j=0;j<72;j++) {
+            if (i == 0) cout << "-";
+            if(i != 0 && i != 31 && j != 0 && j != 71) {
+            	if(im[i-1][j-1] == 1) cout << "*";
+            	else cout << " ";
+			}
+            if (i == 31) cout << "-";
+        }
+		if (i != 0 && i != 31) cout << "|" << endl;
+		else cout << endl; 
+    }
+}
+
+void updateImage(bool im[][M],int s,int x,int y) {
+    for(int i=0;i<30;i++) {
+        for(int j=0;j<70;j++) {
+            if (sqrt(pow(i-double(x),2)+pow(j-double(y),2)) <= s-1) im[i][j] = '*';
+        }
+    }
+}
